361f1069e6ac2aa92524a56cc25752e395c44eb5
Dmytro Borshchanenko <gusto@Dmytros-MacBook-Pro-2.local>
2018-09-24 21:24:25 +0300

    Initial commit

---
+++ spaghetti-monster/anduril/build-all.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+UI=anduril
+
+for TARGET in \
+  BLF_GT \
+  BLF_GT_MINI \
+  BLF_Q8 \
+  EMISAR_D1 \
+  EMISAR_D1S \
+  EMISAR_D4 \
+  EMISAR_D4S \
+  EMISAR_D4S_219c \
+  FW3A \
+  ; do
+  echo "===== $TARGET ====="
+  ../../bin/build-85.sh "$UI" "-DFSM_${TARGET}_DRIVER"
+  mv -f "$UI".hex "$UI".$TARGET.hex
+done
